3015192d1af577ee6a97ee8042d7230287bf1bc7
diih <diih@pop-os.localdomain>
2023-02-28 19:53:57 -0300

    adding the game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,92 @@
+import { useState, useEffect } from 'react'
 import './App.css'
+import SingleCard from './components/SingleCard';
+
+  const cardImages = [
+    { "src": "/img/helmet-1.png", matched:false },
+    { "src": "/img/potion-1.png", matched:false },
+    { "src": "/img/ring-1.png"  , matched:false },
+    { "src": "/img/scroll-1.png", matched:false },
+    { "src": "/img/shield-1.png", matched:false },
+    { "src": "/img/sword-1.png" , matched:false }
+  ]
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
+  const shuffleCards = () => 
+  {
+    const shuffledCards = [...cardImages,...cardImages]
+    .sort(() => Math.random() -0.5)
+    .map(card => ({
+      ...card,
+      id: Math.random()
+      }
+    ))
+    
+    setCards(shuffledCards);
+    setTurns(0);
+    
+  }
+
+  const handleChoice = (card) => {       
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);        
+
+  }
+
+  useEffect(() => {    
+    compareChoices(choiceOne, choiceTwo);      
+  }, [choiceOne, choiceTwo])
+  
+  const compareChoices = (choiceOne, choiceTwo) => {
+    if(choiceOne && choiceTwo){
+      if(choiceOne.src === choiceTwo.src && choiceOne.id !== choiceTwo.id ){        
+
+        setCards(prevState => {
+          return prevState.map(card =>{
+            if(card.src === choiceOne.src){
+              return {...card,matched:true}
+            }            
+              return card;            
+          })
+        })                
+        
+      } 
+      
+        setTimeout(() => {
+          resetTurn();
+        }, 1000); 
+                               
+    }
+    
+  }
+
+  const resetTurn = () => {
+    setTurns(prev => prev+1)
+    setChoiceOne(null);
+    setChoiceTwo(null);
+  }
+  
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <h3>Turns: {turns}</h3>
+      <button onClick={shuffleCards} >New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard 
+            card={card} 
+            key={card.id} 
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}/>
+          )          
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SingleCard.css'
+
+export default function SingleCard({card, handleChoice, flipped}) {
+
+    const handleClick = () => {
+        handleChoice(card);
+    }
+    
+  return (
+    <div className='card'>
+            <div class={flipped ? "flipped" : ""}>
+              <img className="front" src={card.src} alt="card front"></img>
+              <img className="back" src='/img/cover.png' alt='card back' onClick={handleClick}></img>
+            </div>
+          </div>
+  )
+}
